2f2396f673c71c0e7e17ce3d3462a432edf254ce
Jan Fader <jan.fader@web.de>
2020-12-31 00:20:37 +0100

    add lock.sh for screensaver

---
+++ bin/lock.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# original source: https://github.com/petvas/i3lock-blur
+TMPBG=/tmp/screen.png
+RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
+
+ffmpeg -f x11grab -video_size "$RES" -y -i "$DISPLAY" -filter_complex "boxblur=10:1" -vframes 1 $TMPBG -loglevel quiet
+
+# drop all cached passwords for gpg-keys
+gpg-connect-agent reloadagent /bye
+
+# disable notifications but queue them
+dunstctl set-paused true
+
+i3lock -i ${TMPBG} "$@"
+dunstctl set-paused false
+rm ${TMPBG}
